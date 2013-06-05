68f357d49ca6424ecc950c05ec0ab05fbacbd713
McBen <viertelvor12@gmx.net>
2013-06-05 23:30:13 +0200

    fixed broken dds

--- source/lib/dds.h
+++ source/lib/dds.h
@@ -26,8 +26,8 @@ struct DDS_PIXELFORMAT
 
 #ifndef MAKEFOURCC
   #define MAKEFOURCC(ch0, ch1, ch2, ch3) \
-    ((uint32_t)(uint8_t)(ch0) | ( (uint32_t)(uint8_t)(ch1) <8) | \
-    ((uint32_t)(uint8_t)(ch2) <16) | ( (uint32_t)(uint8_t)(ch3) <24))
+    ((uint32_t)(uint8_t)(ch0) | ( (uint32_t)(uint8_t)(ch1) <<8) | \
+    ((uint32_t)(uint8_t)(ch2) <<16) | ( (uint32_t)(uint8_t)(ch3) <<24))
 #endif //defined(MAKEFOURCC)
 
 #define DDS_FOURCC 0x00000004  // DDPF_FOURCC
